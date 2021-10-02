66943dede480584f3de2e029214f1feceb3b14a3
YutaUra <yuuta3594@outlook.jp>
2021-10-03 00:12:52 +0900

    fix

--- src/configs/react-native.ts
+++ src/configs/react-native.ts
@@ -1,8 +1,5 @@
 export default {
-  extends: [
-    // '@react-native-community',
-    'plugin:team-vals/react',
-  ],
+  extends: ['@react-native-community', 'plugin:team-vals/react'],
   rules: {
     // eslint
     'no-undef': 'off',
