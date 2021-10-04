f98e2b6daf132b9c9e77f1914c76080bad268059
YutaUra <45471709+YutaUra@users.noreply.github.com>
2021-10-05 07:40:40 +0900

    pretter

--- src/configs/prettier.ts
+++ src/configs/prettier.ts
@@ -1,12 +1,18 @@
 export default {
   extends: ['prettier'],
-  rules: {
-    // react
-    'react/jsx-newline': 'error',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-indent': 'off',
-    'react/jsx-max-props-per-line': 'off',
-    'react/jsx-indent-props': 'off',
-    'react/jsx-max-depth': ['warn', { max: 5 }],
-  },
+  rules: {},
+  overrides: [
+    {
+      "files": ["**/*.tsx", "**/*.jsx"],
+      "rules": {
+        // react
+        'react/jsx-newline': 'error',
+        'react/jsx-one-expression-per-line': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-max-props-per-line': 'off',
+        'react/jsx-indent-props': 'off',
+        'react/jsx-max-depth': ['warn', { max: 5 }],
+      }
+    }
+  ]
 }
